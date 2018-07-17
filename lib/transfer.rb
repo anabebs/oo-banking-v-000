0b03f9e4cad522f742313d0fee24d0521af9f2a5
Ana Bebic <anabebic.x@gmail.com>
2018-07-17 20:13:50 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,8 +17,21 @@ def valid?
 end 
 
 def execute_transaction
-  
-  
-end 
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
 
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
 end
