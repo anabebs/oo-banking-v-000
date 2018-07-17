586ccf853f7fdb1760fe80ef161959ce6affd92b
Ana Bebic <anabebic.x@gmail.com>
2018-07-17 19:59:56 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,30 @@
+require "spec_helper"
+
+
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver
+  
+  
+  def initialize(sender, receiver, status ='pending', amount)
+ @sender = sender
+ @receiver = receiver 
+ @status ='pending'
+ @amount =amount
+end
+
+def deposit(amount)
+  @balance += amount 
+end 
+
+def display_balance
+   "Your balance is $#{self.balance}."
+end 
+
+def valid?
+  self.status == "open" && self.balance > 0 ? true : false
+end 
+
+def close_account
+  self.status = "closed"
+end 
 end
