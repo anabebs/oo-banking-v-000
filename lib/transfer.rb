139e9211a599c9f42a6ceb7700c5495b2656793b
Ana Bebic <anabebic.x@gmail.com>
2018-07-17 20:04:03 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 
 class Transfer
-  attr_accessor :sender, :receiver, :amount
+  attr_accessor :sender, :receiver, :amount, :status
   
   
   def initialize(sender, receiver, amount,status ="pending")
